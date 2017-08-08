a600d883000759a728ec0364489c6c8996ba2313
renuka <renutarse1@gmail.com>
2017-08-08 12:54:24 +0530

    predecr commited

---
+++ predecr/src/predecr/PreDecrement.java
@@ -0,0 +1,12 @@
+package predecr;
+
+public class PreDecrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		System.out.println(i--);
+
+	}
+
+}
